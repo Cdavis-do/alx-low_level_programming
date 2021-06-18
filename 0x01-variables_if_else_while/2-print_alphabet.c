3d4ee347e5c50ba10e7b429bd2e5fe89373f1f08
Cdavis-do <chikelubadavis23@gmail.com>
2021-06-18 14:57:14 +0100

    List the Alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,11 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
  * main - Entry point
  * Return: Always 0 (success)
  */
-int main(void)
-{int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-char alpha = 'a..z';
-putchar(alpha);
+int main()
+{char ch = 'a';
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
 return (0);
 }

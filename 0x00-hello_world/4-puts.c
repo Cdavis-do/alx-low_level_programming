5267d7ca34d5d7a6e4b18f5a0894aedaa1a267f0
Cdavis-do <chikelubadavis23@gmail.com>
2021-06-18 10:19:25 +0100

    C program to print quote

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include<stdio.h>
+/*
+ *main - Entry point
+ *Return - Always 0 (success)
+ */
 int main(void) /*this specifies there are no arguments taken by main*/
 {char Mystr[] = "\"Programming is like building a multilingual puzzle";
 puts(Mystr);

8571d74daec0279b6f0cf60b5faf3c57096ad720
Cdavis-do <chikelubadavis23@gmail.com>
2021-06-18 11:08:17 +0100

    To print standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main (void)
+{
+  char msg[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    size of (msg) -1;
+}

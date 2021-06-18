77f296ef7b7cd1d876c4e35d8565006f5a72461e
Cdavis-do <chikelubadavis23@gmail.com>
2021-06-18 13:06:27 +0100

    C program to quote

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  *
  * Return - Always 0 (success)
  */
-int main(void) /*main - Entry point*/
+int main(void) /** main - Always 0 (success)*/
 {printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }

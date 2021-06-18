7d09c36e8727c91a725629caa586adba2b833121
Cdavis-do <chikelubadavis23@gmail.com>
2021-06-18 14:29:54 +0100

    Print last digit as greater, less or zero

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ else
 if (l < 6)
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 else
-if (l == 0)
+if (l = 0)
 printf("Last digit of %d is %d and is 0\n", n, l);
 return (0);
 }

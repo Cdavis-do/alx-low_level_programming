0e960bc8e232a54eff31a3a23dd940791beaca72
Cdavis-do <chikelubadavis23@gmail.com>
2021-06-19 13:28:05 +0100

    Lowercase alphabet except qe

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{char ad = 'a';
+char fp = 'f';
+char rz = 'r';
+for (ad = 'a'; ad <= 'd'; ad++)
+putchar(ad);
+for (fp = 'f'; fp <= 'p'; fp++)
+putchar(fp);
+for (rz = 'r'; rz <= 'z'; rz++)
+putchar(rz);
+return (0);
+}

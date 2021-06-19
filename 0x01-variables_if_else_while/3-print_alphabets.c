a030f87411a1a15289b13b091c22745f9263f1e2
Cdavis-do <chikelubadavis23@gmail.com>
2021-06-19 12:58:32 +0100

    Lowercase and Uppercase Alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,10 +4,14 @@
  * Return: Always 0 (success)
  */
 int main(void)
-{char ch = 'a'; char gh = 'A';
+{char ch = 'a';
+char gh = 'A';
+char hd = '\n';
 for (ch = 'a'; ch <= 'z'; ch++)
 putchar(ch);
 for (gh = 'A'; gh <= 'Z'; gh++)
 putchar(gh);
+for (hd = '\n'; hd++)
+putchar(hd);
 return (0);
 }

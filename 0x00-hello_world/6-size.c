e2f77a87bf35835b8bddf17e943a20cb160510e6
Cdavis-do <chikelubadavis23@gmail.com>
2021-06-20 17:42:53 +0100

    Size of Data integer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,17 +4,17 @@
  * Return: Always 0 (success)
  */
 int main(void)
-{/*char s;*/
+{char s;
 int a;
 long int b;
 long long int c;
 float d;
-long double f;
-/*printf("size of char = %d bytes\n", sizeof(s));*/
+short f;
+printf("size of char = %d bytes\n", sizeof(s));
 printf("size of int = %d bytes\n", sizeof(a));
 printf("size of long int = %d bytes\n", sizeof(b));
 printf("size of long long int = %d bytes\n", sizeof(c));
 printf("size of float = %d bytes\n", sizeof(d));
-printf("size of long double= %d bytes\n", sizeof(f));
+printf("size of short = %d bytes\n", sizeof(f));
 return (0);
 }

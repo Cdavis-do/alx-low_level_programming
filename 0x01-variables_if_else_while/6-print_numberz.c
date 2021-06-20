ab872b37090a08cea03063f4835e4a79eb0096e4
Cdavis-do <chikelubadavis23@gmail.com>
2021-06-20 18:26:01 +0100

    BAse 10 single digit numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main()
+{int count=1;
+while (count <= 9)
+{putchar("%d ", count);
+count++;
+}
+return (0);
+}

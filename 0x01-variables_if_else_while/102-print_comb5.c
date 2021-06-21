563dba5b2c3c50facbc4fea1da89094f45076b8b
Cdavis-do <chikelubadavis23@gmail.com>
2021-06-21 11:38:52 +0100

    All possible combination of two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point                                                                                                                        
+ * Return: Always 0 (success)                                                                                                                
+ */
+int main(void)
+{int x, y;
+for (x = 0; x != 100; x++)
+for (y = 0; y != 100; y++)
+if (y > x)
+putchar((x / 10) + 48);
+putchar((x % 10) + 48);
+putchar(32);
+putchar((y / 10) + 48);
+putchar((y % 10) + 48);
+if (x != 98 || y != 99)
+putchar(',')
+putchar(32);
+putchar('\n');
+return (0);
+}

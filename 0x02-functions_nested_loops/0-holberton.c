4ad4e346e0fb08f31cedcb2a67d44ae44d377686
Cdavis-do <chikelubadavis23@gmail.com>
2021-06-22 14:53:39 +0100

    Print Holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -4,10 +4,10 @@
  * Return: Always 0 (success)
  */
 #include "holberton.h"
-{int = 0 , j;
- while(i<2)
+{int i = 0 , j;
+ while(i < 2)
    {j = 0
-       while(j<1)
+       while(j < 1)
 	 {printf("Holberton");
 	 }
      printf("\n");

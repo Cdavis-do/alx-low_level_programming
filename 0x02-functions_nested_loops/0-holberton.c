a42b0c35f0206ca0214fd658d71bb1f1dd52876d
Cdavis-do <chikelubadavis23@gmail.com>
2021-06-22 14:40:42 +0100

    Print Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{int = 0 , j;
+ while(i<2)
+   {j = 0
+       while(j<1)
+	 {printf("Holberton");
+	 }
+     printf("\n");
+   }
+ return (0);
+}
